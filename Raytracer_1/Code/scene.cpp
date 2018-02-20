248d8def202cef89a7e4a95310e034812e2135d5
suzannemaquelin <slmaquelin@gmail.com>
2018-02-20 15:40:52 +0100

    assignment 2 intial commit

---
+++ Raytracer_1/Code/scene.cpp
@@ -0,0 +1,134 @@
+#include "scene.h"
+
+#include "hit.h"
+#include "image.h"
+#include "material.h"
+#include "ray.h"
+
+#include <cmath>
+#include <limits>
+
+using namespace std;
+
+Color Scene::trace(Ray const &ray)
+{
+    // Find hit object and distance
+    Hit min_hit(numeric_limits<double>::infinity(), Vector());
+    ObjectPtr obj = nullptr;
+    for (unsigned idx = 0; idx != objects.size(); ++idx)
+    {
+        Hit hit(objects[idx]->intersect(ray));
+        if (hit.t < min_hit.t)
+        {
+            min_hit = hit;
+            obj = objects[idx];
+        }
+    }
+
+    // No hit? Return background color.
+    if (!obj) return Color(0.0, 0.0, 0.0);
+
+    Material material = obj->material;          //the hit objects material
+    Point hit = ray.at(min_hit.t);                 //the hit point
+    Vector N = min_hit.N;                          //the normal at hit point
+    Vector V = -ray.D;                             //the view vector
+
+    /****************************************************
+    * This is where you should insert the color
+    * calculation (Phong model).
+    *
+    * Given: material, hit, N, V, lights[]
+    * Sought: color
+    *
+    * Hints: (see triple.h)
+    *        Triple.dot(Vector) dot product
+    *        Vector + Vector    vector sum
+    *        Vector - Vector    vector difference
+    *        Point - Point      yields vector
+    *        Vector.normalize() normalizes vector, returns length
+    *        double * Color     scales each color component (r,g,b)
+    *        Color * Color      dito
+    *        pow(a,b)           a to the power of b
+    ****************************************************/
+
+//    Color color = material.color;                  // place holder
+
+//    Triple I_a = color.operator*(material.ka);
+//    Triple I_d = Triple();
+//    Triple I_s = Triple();
+
+//    for (int i =0; i < lights.size(); i++) {
+//        Triple L = (lights[i]->position) - (hit);
+//        L.normalize();
+//        I_d += (lights[i]->color) * max(0.0, N.dot(L));
+
+//        double s = N.dot(L) * 2;
+//        Triple R = (N*s) - L;
+//        R.normalize();
+//        double m = max(0.0, R.dot(V));
+//        I_s += lights[i]->color * pow(m, material.n);
+//    }
+//    I_d = I_d * (material.kd) * (material.color);
+//    I_s = I_s * (material.ks) * (material.color);
+//    Triple sum = I_a + I_d + I_s;
+//    sum = I_d;
+//    return sum;
+
+    Color I_d = Color(0.0, 0.0, 0.0);
+
+    for (int i =0; i < lights.size(); i++) {
+        Triple L = (lights[i]->position) - (hit);
+        L = L.normalized();
+        I_d += max(0.0, N.dot(L)) *(lights[i]->color);
+    }
+    I_d = (material.kd) * I_d * (material.color);
+
+    return I_d;
+
+
+
+}
+
+void Scene::render(Image &img)
+{
+    unsigned w = img.width();
+    unsigned h = img.height();
+    for (unsigned y = 0; y < h; ++y)
+    {
+        for (unsigned x = 0; x < w; ++x)
+        {
+            Point pixel(x + 0.5, h - 1 - y + 0.5, 0);
+            Ray ray(eye, (pixel - eye).normalized());
+            Color col = trace(ray);
+            col.clamp();
+            img(x, y) = col;
+        }
+    }
+}
+
+// --- Misc functions ----------------------------------------------------------
+
+void Scene::addObject(ObjectPtr obj)
+{
+    objects.push_back(obj);
+}
+
+void Scene::addLight(Light const &light)
+{
+    lights.push_back(LightPtr(new Light(light)));
+}
+
+void Scene::setEye(Triple const &position)
+{
+    eye = position;
+}
+
+unsigned Scene::getNumObject()
+{
+    return objects.size();
+}
+
+unsigned Scene::getNumLights()
+{
+    return lights.size();
+}
---
+++ Raytracer_1/Code/shapes/sphere.cpp
@@ -0,0 +1,62 @@
+#include "sphere.h"
+
+#include <cmath>
+
+using namespace std;
+
+Hit Sphere::intersect(Ray const &ray)
+{
+    /****************************************************
+    * RT1.1: INTERSECTION CALCULATION
+    *
+    * Given: ray, position, r
+    * Sought: intersects? if true: *t
+    *
+    * Insert calculation of ray/sphere intersection here.
+    *
+    * You have the sphere's center (C) and radius (r) as well as
+    * the ray's origin (ray.O) and direction (ray.D).
+    *
+    * If the ray does not intersect the sphere, return false.
+    * Otherwise, return true and place the distance of the
+    * intersection point from the ray origin in *t (see example).
+    ****************************************************/
+    double t;
+    Triple c = this->position;
+    double r = this->r;
+    Triple o = ray.O;
+    Triple l = ray.D;
+
+    double term1 = l.dot(o.operator-(c));
+    double term2 = (o.operator-(c)).length_2();
+    double discriminant = term1*term1 - term2 + r*r;
+
+    if(discriminant < 0) {
+        return Hit::NO_HIT();
+    }
+
+    t = -term1 + sqrt(discriminant);
+//    return Hit::
+
+
+    /****************************************************
+    * RT1.2: NORMAL CALCULATION
+    *
+    * Given: t, C, r
+    * Sought: N
+    *
+    * Insert calculation of the sphere's normal at the intersection point.
+    ****************************************************/
+
+    Triple intersection = o.operator+(l.operator*(t));
+    Vector N = intersection.operator-(c);
+    N.normalize();
+
+    return Hit(t,N);
+}
+
+Sphere::Sphere(Point const &pos, double radius)
+:
+    position(pos),
+    r(radius)
+{}
