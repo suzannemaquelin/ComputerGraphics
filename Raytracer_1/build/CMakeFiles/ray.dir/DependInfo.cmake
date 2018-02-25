0491eed68f24dfff7e62be0d3afd5ffd9831c86e
Suzanne Maquelin <slmaquelin@gmail.com>
2018-02-25 14:53:38 +0100

    cleaned cmake files

---
+++ Raytracer_1/build/CMakeFiles/ray.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/Suzanne/Documents/informatica/CG/Raytracer_1/Code/image.cpp" "/Users/Suzanne/Documents/informatica/CG/Raytracer_1/build/CMakeFiles/ray.dir/Code/image.cpp.o"
+  "/Users/Suzanne/Documents/informatica/CG/Raytracer_1/Code/lode/lodepng.cpp" "/Users/Suzanne/Documents/informatica/CG/Raytracer_1/build/CMakeFiles/ray.dir/Code/lode/lodepng.cpp.o"
+  "/Users/Suzanne/Documents/informatica/CG/Raytracer_1/Code/main.cpp" "/Users/Suzanne/Documents/informatica/CG/Raytracer_1/build/CMakeFiles/ray.dir/Code/main.cpp.o"
+  "/Users/Suzanne/Documents/informatica/CG/Raytracer_1/Code/objloader.cpp" "/Users/Suzanne/Documents/informatica/CG/Raytracer_1/build/CMakeFiles/ray.dir/Code/objloader.cpp.o"
+  "/Users/Suzanne/Documents/informatica/CG/Raytracer_1/Code/raytracer.cpp" "/Users/Suzanne/Documents/informatica/CG/Raytracer_1/build/CMakeFiles/ray.dir/Code/raytracer.cpp.o"
+  "/Users/Suzanne/Documents/informatica/CG/Raytracer_1/Code/scene.cpp" "/Users/Suzanne/Documents/informatica/CG/Raytracer_1/build/CMakeFiles/ray.dir/Code/scene.cpp.o"
+  "/Users/Suzanne/Documents/informatica/CG/Raytracer_1/Code/shapes/example.cpp" "/Users/Suzanne/Documents/informatica/CG/Raytracer_1/build/CMakeFiles/ray.dir/Code/shapes/example.cpp.o"
+  "/Users/Suzanne/Documents/informatica/CG/Raytracer_1/Code/shapes/plane.cpp" "/Users/Suzanne/Documents/informatica/CG/Raytracer_1/build/CMakeFiles/ray.dir/Code/shapes/plane.cpp.o"
+  "/Users/Suzanne/Documents/informatica/CG/Raytracer_1/Code/shapes/quad.cpp" "/Users/Suzanne/Documents/informatica/CG/Raytracer_1/build/CMakeFiles/ray.dir/Code/shapes/quad.cpp.o"
+  "/Users/Suzanne/Documents/informatica/CG/Raytracer_1/Code/shapes/sphere.cpp" "/Users/Suzanne/Documents/informatica/CG/Raytracer_1/build/CMakeFiles/ray.dir/Code/shapes/sphere.cpp.o"
+  "/Users/Suzanne/Documents/informatica/CG/Raytracer_1/Code/shapes/triangle.cpp" "/Users/Suzanne/Documents/informatica/CG/Raytracer_1/build/CMakeFiles/ray.dir/Code/shapes/triangle.cpp.o"
+  "/Users/Suzanne/Documents/informatica/CG/Raytracer_1/Code/triple.cpp" "/Users/Suzanne/Documents/informatica/CG/Raytracer_1/build/CMakeFiles/ray.dir/Code/triple.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
